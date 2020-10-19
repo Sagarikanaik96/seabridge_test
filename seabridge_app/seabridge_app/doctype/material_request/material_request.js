09d8253ca20e909fe169e1cb6bf049d2759d9ac2
Subramani A C <subramani.ac@promantia.com>
2020-10-19 11:30:50 +0530

    [7433/7787/custom]7743hiding the fields based on item group,7787creating the button and checking for the existing blanket order

--- seabridge_app/seabridge_app/doctype/material_request/material_request.js
+++ seabridge_app/seabridge_app/doctype/material_request/material_request.js
@@ -17,5 +17,50 @@ frappe.ui.form.on('Material Request', {
                 msgprint('Unable to save the '+frm.doc.doctype+' as the naming series are unavailable. Please provide the naming series at the Company: '+frm.doc.company+' to save the document.','Alert')
             }
         })
+    },
+    refresh: function(frm,cdt,cdn) {
+        var blanket_order_type
+        var company
+        var flag = 0;
+        var check=0;
+        if(frm.doc.docstatus === 1){
+            frappe.call({
+                method: "frappe.client.get_list",
+                async:false,
+                args: {
+                    doctype: "Blanket Order",
+                    fields : ["company","blanket_order_type","name"],
+                    filters:{
+                        "blanket_order_type":"Purchasing",
+                        "company" : frm.doc.company
+                    }
+                },
+                callback: function(r) {
+                    
+                    for(var i=0;i<r.message.length;i++){
+                        blanket_order_type=r.message[i].blanket_order_type
+                        company=r.message[i].company
+                        var name = r.message[i].name
+                        frappe.model.with_doc("Blanket Order", name, function() {
+                            var tabletransfer= frappe.model.get_doc("Blanket Order", name)
+                            $.each(frm.doc.items, function(idx, item){
+                                $.each(tabletransfer.items, function(index, row){
+                                    if(item.item_code== row.item_code){
+                                        frm.add_custom_button(__('View Blanket Order'), function(){
+                                            frappe.route_options = {"company": frm.doc.company, "blanket_order_type": "Purchasing","item_code":item.item_code}
+                                            frappe.set_route("List", "Blanket Order");
+                                        });
+                                        check++;
+                                    }
+                                })
+                            })
+                        })
+                    }
+                    if(check>0){frappe.msgprint('Blanket Orders available for the requested Items.<br /> Select the View Blanket Orders button to view the Blanket Purchase Orders.')}
+                }
+            })
+        }    
     }
+        
 })
+
