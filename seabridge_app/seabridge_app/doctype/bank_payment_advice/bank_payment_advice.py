45ca9918aa3c9be6eefd296ee032990059bda69d
veena56 <veena.h@promantia.com>
2020-11-24 17:34:41 +0530

    [8003]/creating a payment entry by clubbing the purchase invoice of same supplier

--- seabridge_app/seabridge_app/doctype/bank_payment_advice/bank_payment_advice.py
+++ seabridge_app/seabridge_app/doctype/bank_payment_advice/bank_payment_advice.py
@@ -6,35 +6,45 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from datetime import date
+from itertools import groupby
 
 class BankPaymentAdvice(Document):
 	pass
 
 def auto_create_payment_entry(doc,method): 
 	bpad=frappe.db.get_list("Bank Payment Advice Details",filters={'parent':doc.name,'parenttype':'Bank Payment Advice'},fields={'*'})
-	for row in bpad:
+	for key, group in groupby(bpad, key=lambda x: (x['supplier_name'])):
+		grouped_by_supplier= list(group)
 		pe_doc=frappe.get_doc(dict(doctype = 'Payment Entry',
 		payment_type="Pay",
 		posting_date=doc.date,
 		company=doc.company,
 		mode_of_payment="Wire Transfer",
-		paid_to=frappe.db.get_value('Purchase Invoice',{'name':row.invoice_document},'credit_to'),
 		paid_from=frappe.db.get_value('Bank Account',{'name':doc.bank_account},'account'),
 		party_type="Supplier",
-		party=row.supplier_name,
-		paid_amount=row.payment_transaction_amount,
-		received_amount=55.5,
+		paid_amount="0",
+		received_amount="0",
+		party=key,
 		reference_no=doc.name,
 		reference_date=date.today()
 		)).insert(ignore_mandatory=True)
-		
-		pe_doc.append('references', {
-			'reference_doctype':"Purchase Invoice",
-			'reference_name':row.invoice_document,
-			'due_date':row.due_date,
-			'total_amount':row.invoice_amount,
-			'outstanding_amount':row.outstanding_amount-row.payment_transaction_amount,
-			'allocated_amount':row.payment_transaction_amount	
-						    
-						})
+
+		amount=0
+		for row in grouped_by_supplier:	
+			amount=amount+row.payment_transaction_amount
+			pe_doc.update({
+                "paid_to":frappe.db.get_value('Purchase Invoice',{'name':row.invoice_document},'credit_to'),
+                "paid_amount":amount,
+				"received_amount":amount,
+            })
+
+			pe_doc.append('references', {
+				'reference_doctype':"Purchase Invoice",
+				'reference_name':row.invoice_document,
+				'due_date':row.due_date,
+				'total_amount':row.invoice_amount,
+				'outstanding_amount':row.outstanding_amount-row.payment_transaction_amount,
+				'allocated_amount':row.payment_transaction_amount	
+								
+							})
 		pe_doc.submit()
