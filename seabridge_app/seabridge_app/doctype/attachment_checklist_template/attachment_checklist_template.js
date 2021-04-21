d025d9518a24991c18db4b16375d74645c0ba67e
sagarikanaik <sagarika.naik@promantia.com>
2021-04-21 13:24:56 +0530

    8780/Added custom doctype for attachment checklist

---
+++ seabridge_app/seabridge_app/doctype/attachment_checklist_template/attachment_checklist_template.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, seabridge_app and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Attachment Checklist Template', {
+	// refresh: function(frm) {
+
+	// }
+});
