cdab7f481e76709fb63475f9294bf0536407b883
sagarikanaik <sagarika.naik@promantia.com>
2021-07-28 11:35:34 +0530

    Bug fix for Customer permission

--- seabridge_app/seabridge_app/doctype/customer/customer.js
+++ seabridge_app/seabridge_app/doctype/customer/customer.js
@@ -3,19 +3,21 @@
 
 frappe.ui.form.on('Customer',{
 	after_save:function(frm,cdt,cdn){
+		var represents_company=""
 		if(frm.doc.represents_company){
+			represents_company=frm.doc.represents_company
+		}
 			frappe.call({
 					method:"seabridge_app.seabridge_app.doctype.customer.customer.create_permissions",
 					args:{
 						name:frm.doc.name,
-						represents_company:frm.doc.represents_company
+						represents_company:represents_company
 					},
 					async:false,
 					callback: function(r){
 						window.location.reload	 	
 					}
 				});
-		}
 	},
 	validate: function (frm, cdt, cdn) {
 		if (frm.doc.represents_company == "None" || frm.doc.represents_company == '') {
